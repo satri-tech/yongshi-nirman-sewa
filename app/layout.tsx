c3dc61a4a097f3580da341e6a2edd12df2cfa4df
Samarth Technical Research and Innovations <tech.satri@gmail.com>
2025-10-18 19:35:02 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
       >
         <SessionProviderWrapper>
           <Toaster />
-          l
           {children}
         </SessionProviderWrapper>
       </body>
